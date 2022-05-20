1b1e8a02ca94bfe206d9f357a356cd0b647f7208
VINICIUS DO AMARAL SOUZA <vinicius.asouza34@senacsp.edu.br>
2022-05-19 21:54:56 -0300

    Classe em PHP

---
+++ aula12/poo/Calculadora.class.php
@@ -0,0 +1,55 @@
+<?php
+
+class Calculadora{
+
+    private ?int $n1;
+    private ?int $n2;
+
+    public function __construct($n1,$n2)
+    {
+         $this->n1 = $n1;
+         $this->n2 = $n2;
+    }
+
+    public function soma():int
+    {
+        return $this->n1 + $this->n2;
+    }
+
+    public function subtracao():int
+    {
+        return $this->n1 - $this->n2;
+    }
+
+    public function __destruct()
+    {
+        echo "Obrigado pela visita";
+    }
+}
+
+//unset() = destroi qualquer variavel $
+
+/*
+__ = metodos magico e constantes magicas
+    __construct()
+    __FILE__ = Nome do Arquivo
+
+
+Sintaxe Metodo
+    <visibilidade> <tipoDeDado> <nomeDaVariavel>
+
+Sintaxe Function
+    <visibilidade> function <nomeFunction>():TipoDeDadoDeRetorno
+
+
+visibilidade
+
+    Private
+        So é possivel acessar o atributo ou metodo dentro da propria classe
+
+    Protected
+        É possivel acessar o atributo ou metodo dentro da propria classe ou qualquer classe tenha herdado
+
+    Public
+        É possivel acessar o atributo ou metodo de qualquer lugar 
+*/
---
+++ aula12/poo/poo.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once 'Calculadora.class.php';
+
+class Main{
+    private ?int $num1;
+    private ?int $num2;
+    private Calculadora $calc;
+
+    public function __construct() 
+    {
+        $resultado = null;
+
+        $this->num1 = isset($_POST['num1']) ? (int) $_POST['num1'] : null;
+        $this->num2 = isset($_POST['num2']) ? (int) $_POST['num2'] : null;
+
+        $this->calc = new Calculadora($this->num1, $this->num2);
+
+
+        try{
+            $resultado = $this->operacao();
+
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
+
+        $this->front($this->num1, $this->num2, $resultado);
+    }
+
+    private function operacao():?float
+    {
+
+        switch ($_POST['operacao'] ?? '') {
+            case 'soma':
+                return $this->calc->soma();
+                break;
+
+            case 'subtracao':
+                return $this->calc->subtracao();
+                break;
+            
+            default:
+                throw new Exception('Não é possivel executar essa operação');
+        }
+    }
+
+    private function front(?int $n1, ?int $n2, ?int $resultado = null):void
+    {
+        include 'front.php';
+    }
+}
+
+new Main;
+
+/*
+    Type Casting (int) = Manipulacao de tipos
+
+
+        ?num1
+        ?num2
+        :?bool
+
+*/
